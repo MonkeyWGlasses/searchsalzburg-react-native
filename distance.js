f57fc803ff50db69b6a6253d265c0ad26d182397
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2016-04-10 16:08:07 +0200

    add current distance to listview

---
+++ distance.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  View,
+  Text,
+  Component,
+} = React;
+
+
+var geolib = require('geolib');
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#414141',
+  },
+  distance: {
+    fontFamily: 'Helvetica',
+    fontWeight: '100',
+    color: 'orange',
+    fontSize: 11,
+    marginLeft: 8,
+    marginTop: 2,
+    textAlign: 'right',
+  }
+});
+
+class Distance extends Component {
+
+  constructor (props) {
+      super(props);
+      this.state = {
+        targetPositon: {
+          latitude: this.props.position[1],
+          longitude: this.props.position[0],
+        },
+        distance: 0,
+      }
+  }
+
+  componentDidMount() {
+   navigator.geolocation.getCurrentPosition(
+     (position) => {
+       var initialPosition = position;
+       var distance = geolib.getDistance(position.coords,this.state.targetPositon)
+       this.setState({ distance: distance})
+    },
+     (error) => console.log(error.message),
+     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+   );
+  }
+
+  calculateDistance() {
+    return geolib.getDistance(this.state.initialPosition.coords,this.state.targetPositon)
+  }
+
+  render() {
+    var distance = (this.state.distance/1000).toFixed(2);
+    return (<Text style={styles.distance}>{distance} km</Text>);
+  }
+}
+
+
+module.exports = Distance;
--- list.js
+++ list.js
@@ -17,11 +17,20 @@ var {
 // var Section = TableView.Section;
 // var Item = TableView.Item;
 var detail = require('./detail');
-
+var Distance = require('./distance');
 
 var styles = StyleSheet.create({
   rowContainer: {
     height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  left:{
+    flex: 2,
+  },
+  right:{
+    flex: 1,
+    marginRight: 10,
   },
   title: {
     fontFamily: 'Helvetica',
@@ -55,30 +64,6 @@ class List extends React.Component{
         currentOS: this.props.currentOS,
       }
   }
-  //
-  // handleChangePageAndroid(rowData) {
-  //   this.props.onForward(rowData);
-  // }
-  //
-  // handleChangePageIOS(rowData) {
-  //   this.props.navigator.push({
-  //     title: rowData.properties.ADRESSE,
-  //     component: detail,
-  //     passProps:{
-  //       toggleNavBar: this.props.toggleNavBar,
-  //       currentParkingSpace: rowData,
-  //     }
-  //   })
-  // }
-  //
-  // handleChangePage(rowData) {
-  //   if(this.state.currentOS == "android"){
-  //     return this.handleChangePageAndroid(rowData)
-  //   } else {
-  //     return this.handleChangePageIOS(rowData)
-  //   }
-  // }
-  //
 
   render() {
     return (
@@ -93,30 +78,17 @@ class List extends React.Component{
     return (
       <TouchableHighlight underlayColor='#D9D9D9' onPress={ () => this.props.onForward(rowData) }>
         <View style={styles.rowContainer}>
-          <Text style={styles.title}>{rowData.properties.ADRESSE}</Text>
-          <Text style={styles.subtitle}>Anzahl {rowData.properties.ANZAHL_PLAETZE}</Text>
+          <View style={styles.left}>
+            <Text style={styles.title}>{rowData.properties.ADRESSE}</Text>
+            <Text style={styles.subtitle}>Anzahl {rowData.properties.ANZAHL_PLAETZE}</Text>
+          </View>
+          <View style={styles.right}>
+            <Distance position={rowData.geometry.coordinates}/>
+          </View>
         </View>
       </TouchableHighlight>
     );
   }
 }
-
-// <ListView
-// dataSource={this.state.dataSource}
-// renderRow={this.renderRow}
-// />
-// <TableView style={{flex:1,paddingTop: 35}}
-//                  allowsToggle={true}
-//                  allowsMultipleSelection={true}
-//                  tableViewStyle={TableView.Consts.Style.Grouped}
-//                  tableViewCellStyle={TableView.Consts.CellStyle.Value1}
-//                  onPress={this.handleChangePage.bind(this)}>
-//           <Section arrow={true}>
-//               <Item value="1" detail="Anzahl 3" >Item 1</Item>
-//               <Item value="2" detail="Anzahl 2" >Item 3</Item>
-//               <Item value="2" detail="Anzahl 2" >Item 2</Item>
-//           </Section>
-//       </TableView>
-
 
 module.exports = List;
