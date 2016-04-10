a196e51b06aebde3ae3a8b6905ffcd52ba5d6953
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2016-04-10 14:09:11 +0200

    add imprint infos

--- imprint.js
+++ imprint.js
@@ -5,34 +5,62 @@ var {
   StyleSheet,
   View,
   Text,
-  Component
+  Component,
+  MapView,
+  Linking,
+  TouchableNativeFeedback,
+  TouchableHighlight
 } = React;
 
 var styles = StyleSheet.create({
-  description: {
-    fontSize: 20,
+  headline: {
+    fontSize: 35,
     textAlign: 'center',
-    color: '#FFFFFF'
+  },
+  subline: {
+    marginTop: 10,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  info: {
+    marginTop: 7,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  email: {
+    marginTop: 3,
+    fontSize: 11,
+    textAlign: 'center',
   },
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#123456',
+    backgroundColor: '#FFFFFF',
+    padding: 5,
+  },
+  map: {
+    flex: 1,
+    alignSelf: 'stretch',
   }
 });
 
 class Imprint extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          Entwickler:
-          Simon HintersonnleitnerTESTESTEST
-        </Text>
-      </View>
+       <View style={styles.container}>
+         <Text style={styles.headline}>Impressum</Text>
+         <Text style={styles.subline}>Diese App wurde im Rahmen des Praktischen Teils der Bachelorarbeit 2 an der Fachhochschule Salzburg erstellt.</Text>
+         <Text style={styles.info}>Datenquelle: Open-Data Portal data.gv.at</Text>
+         <Text style={styles.info}>Simon Hintersonnleitner</Text>
+         <Text style={styles.email}>shintersonnleitner.mmt-b2013@fh-salzburg.ac.at</Text>
+       </View>
+
     );
   }
 }
+
 
 module.exports = Imprint;
